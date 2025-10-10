b948ebdb16b74b3f20e124c67871544aed695bac
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-10 21:43:46 +0530

    update commit

---
+++ src/com/jspiders/example/Mainclass5.java
@@ -0,0 +1,22 @@
+package com.jspiders.example;
+
+class Employee{
+	int eid=101;
+	@Override
+	public boolean equals(Object obj) {
+		Employee e = (Employee)obj;
+		if(this.eid == e.eid) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+public class Mainclass5 {
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		Employee e2 = new Employee();
+		System.out.println(e1.equals(e2));
+	}
+}
