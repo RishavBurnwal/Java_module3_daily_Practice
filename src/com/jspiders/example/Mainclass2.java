ac85138b93caa29a12e4078b66e3418110182fae
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-10 21:19:37 +0530

    Initial commit

---
+++ src/com/jspiders/example/Mainclass2.java
@@ -0,0 +1,21 @@
+package com.jspiders.example;
+class Student1{
+	String name = "Rama";
+}
+class Student2{
+	String name = "Rama";
+	@Override
+	public String toString() {
+		return "Student Name: " + name;	
+	}
+}
+public class Mainclass2 {
+	public static void main(String[] args) {
+		Student1 obj = new Student1();
+		System.out.println(obj);
+		
+		Student2 ref = new Student2();
+		System.out.println(ref);
+	}
+
+}
