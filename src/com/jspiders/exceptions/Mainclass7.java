c58d350cff307aee30e3453485c9b7ab98033a3d
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-16 23:34:39 +0530

    new commit

--- src/com/jspiders/exceptions/Mainclass7.java
+++ src/com/jspiders/exceptions/Mainclass7.java
@@ -1,5 +1,18 @@
 package com.jspiders.exceptions;
-
 public class Mainclass7 {
-
+	static void test() {
+		System.out.println("test starts");
+		System.out.println(10/0);
+		System.out.println("test ends");
+	}
+	static void disp() {
+		System.out.println("disp starts");
+		test();
+		System.out.println("disp ends");
+	}
+	public static void main(String[] args) {
+		System.out.println("main starts");
+		disp();
+		System.out.println("main ends");
+	}
 }
