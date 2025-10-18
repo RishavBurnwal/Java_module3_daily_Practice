840c1178f75eef434a3422b2257cb53340551fb5
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-18 23:37:47 +0530

    commit changes

--- src/com/jspiders/exceptions/Mainclass13.java
+++ src/com/jspiders/exceptions/Mainclass13.java
@@ -1,5 +1,20 @@
 package com.jspiders.exceptions;
 
 public class Mainclass13 {
+	public static void main(String[] args) {
+		System.out.println("program starts");
+		String str = "Java";
+		try {
+			System.out.println(str.charAt(7));
+		}
+		catch(NullPointerException e){
+			e.printStackTrace();
+		}
+		catch (StringIndexOutOfBoundsException e) {
+			String msg = e.getMessage();
+			System.out.println(msg);
+		}
+		System.out.println("Program Ends");
+	}
 
 }
