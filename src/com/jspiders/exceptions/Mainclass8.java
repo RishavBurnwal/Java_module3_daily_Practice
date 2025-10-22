b742325291f1ce7053ed633200b3b5016a43b58b
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-22 23:32:13 +0530

    new commit

--- src/com/jspiders/exceptions/Mainclass8.java
+++ src/com/jspiders/exceptions/Mainclass8.java
@@ -7,6 +7,27 @@ class MyCustomException extends Exception{
 		super(msg);
 	}
 }
+class Trigger{
+	static void verify(int num) throws MyCustomException{
+		if(num % 2 == 0 && num % 3 == 0) {
+			MyCustomException obj = new MyCustomException("Divisible by 2 & 3");
+			throw obj;
+		}
+		else {
+			System.out.println("All is well");
+		}
+	}
+}
 public class Mainclass8 {
+	public static void main(String[] args) {
+		try {
+			Trigger.verify(10);
+			Trigger.verify(15);
+			Trigger.verify(12);
+		}
+		catch(MyCustomException e) {
+			System.out.println("Exception occurred "+ e.getMessage());
+		}
+	}
 
 }
