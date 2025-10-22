acb5441cc861b0848961f900fa69b8fdd9af03e5
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-22 23:34:05 +0530

    new program commiting

--- src/com/jspiders/exceptions/Mainclass1.java
+++ src/com/jspiders/exceptions/Mainclass1.java
@@ -1,5 +1,15 @@
 package com.jspiders.exceptions;
 
 public class Mainclass1 {
+	public static void main(String[] args) {
+		System.out.println("Program Starts");
+		try {
+			System.out.println(10/0);
+		}
+		catch (ArithmeticException e) {
+			System.out.println("Divided by zero");
+		}
+		System.out.println("Program Ends");
+	}
 
 }
