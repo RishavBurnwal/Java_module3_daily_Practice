8cfd55287100ae7c4be6520f852b27329c7dc6e9
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-11-01 01:31:14 +0530

    new program

--- src/com/jspiders/filehandling/Mainclass14.java
+++ src/com/jspiders/filehandling/Mainclass14.java
@@ -1,5 +1,24 @@
 package com.jspiders.filehandling;
+import java.io.File;
+import java.io.IOException;
 
 public class Mainclass14 {
+	public static void main(String[] args) {
+		File ref = new File("C:/File_Handling/FILEIO/index.txt");
+		boolean flag = ref.exists();
+		if(flag == false) {
+			try {
+				ref.createNewFile();
+				System.out.println("File Created");
+				ref.setReadable(false);
+				ref.setWritable(false);
+				ref.setExecutable(false);
+				System.out.println("Permission Granted");
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
