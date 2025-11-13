7b4994c1b511ca298b5aed9db51f2bf35a9e7da8
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-11-13 23:16:10 +0530

    commit program

--- src/com/jspiders/ArrayList/MainProduct.java
+++ src/com/jspiders/ArrayList/MainProduct.java
@@ -6,11 +6,11 @@ import java.util.Iterator;
 public class MainProduct {
 	public static void main(String[] args) {
 		ArrayList<Product> al =new  ArrayList<Product>;
-		al.add(new product(2, "Laptop" , 51000));
+		al.add(new Product(2, "Laptop" , 51000));
 		
 		
-		collection.sort(al);
-		Iterator<E> i = al.iterator();
+		Collections.sort(al);
+		Iterator i = al.iterator();
 		while(i.hasNext()) {
 			System.out.println(i.next());
 		}
