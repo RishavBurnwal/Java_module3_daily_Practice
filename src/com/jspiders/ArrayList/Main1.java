1f0a9c578ff2e51b80d2a75258576bb1a9370e68
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-11-15 23:49:29 +0530

    commit changes

--- src/com/jspiders/ArrayList/Main1.java
+++ src/com/jspiders/ArrayList/Main1.java
@@ -7,7 +7,7 @@ public class Main1{
 		Student s1 = new Student(1, "Rishav", 23);  // Student class
 		Student s2 = new Student(2, "Prince", 24);
 		
-		ArrayList<Student> al = new ArrayList();
+		ArrayList<Student> al = new ArrayList();  // <> --> this is known as generics 
 		al.add(s1);
 		al.add(s2);
 		al.add(new Student(3, "Ajay", 22));
